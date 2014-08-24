ec7334a6ab41c6cd85e06246965ff08a75d2c682
JuidthBK <judith.buendgenskosten@gmail.com>
2014-08-24 10:46:18 +0200

    Version 1
    some comments (need to be corrected), some code

--- cachematrix.R
+++ cachematrix.R
@@ -3,16 +3,44 @@
 ## cacheSolve does....
 
 
+## Creates a matrix that is the inverse of X (does the actual caching)
+
 makeCacheMatrix <- function(x = matrix()) {
 ## x is the matrix you want to work with
+    
+    
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    ##this takes the vector or matrix y, and saves it under "x". This leaves us with x that we can manipulate, while y is protected/not touched
+    
+    get <- function() x
+    ## if you call the get function, it will just provide you with the value of x
+    setinverse <- function(solve) m <<- solve
+    ##setinverse is the function that does the actual inversion
+    
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
     
     
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        ## As above, x is the matrix you want to work with
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+## can't use $ here, unless I have names in the matrix, which I do not...
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
